c3bf7734f3d69958971f08cf6359c164b1625323
slayman111 <77606207+slayman111@users.noreply.github.com>
2021-09-11 15:49:53 +0300

    Add files via upload

---
+++ FrameExample/FrameExample/Views/Pages/NewsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FrameExample
+{
+    /// <summary>
+    /// Логика взаимодействия для NewPage.xaml
+    /// </summary>
+    public partial class NewsPage : Page
+    {
+        public NewsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHeigth_Click(object sender, RoutedEventArgs e)
+        {
+            image.Visibility = Visibility.Hidden;
+        }
+    }
+}
